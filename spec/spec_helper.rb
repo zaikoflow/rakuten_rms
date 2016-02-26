d9679a9b2217cd7d1f3c9600977e88b4b661395d
camelmasa <camelmasa@gmail.com>
2016-02-26 22:44:18 +0900

    Display coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,21 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'vcr'
 require 'webmock'
+require "codeclimate-test-reporter"
+require 'simplecov'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.ignore_hosts 'codeclimate.com'
+end
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
 end
 
 require 'rakuten_rms'
